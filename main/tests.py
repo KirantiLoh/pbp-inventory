b5850f3321c782736349bf62266080b94701bcc2
Maurice Yang <mauriceyangmy28@gmail.com>
2023-09-13 00:38:30 +0700

    feat: Added testing

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class MainTest(TestCase):
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main.html')
